b55f0bf0e3f5525fb54f80b626561bbad9ff1eb9
zahiruldu <zahirul.arb@gmail.com>
2020-08-09 23:53:50 +0600

    DEV: index

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+
+import { configureStore } from './state/store';
+
+const initialState = {};
+
+const store = configureStore(initialState);
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+  </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
